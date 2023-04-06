cc0865c20280b94e5c8194fabc670819d2998461
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-04-07 00:22:28 +0400

    Typed. Destructured the props. Implemented deleteCallback . Pulled out the data.

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -1,49 +1,41 @@
 import React from 'react'
-import { AffairType } from '../../HW2'
+import {AffairType} from '../../HW2'
 import s from './Affair.module.css'
 import s2 from '../Affairs.module.css'
 
+// Тип пропсов компоненты Affair
 type AffairPropsType = {
-    // key не нужно типизировать
-    affair: AffairType
-    deleteAffairCallback: any // need to fix any
+   affair: AffairType
+   deleteAffairCallback: (_id: number) => void
 }
 
-function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {
-        // need to fix
-    }
+export const Affair: React.FC<AffairPropsType> = ({affair, deleteAffairCallback}) => {
+   // колбэк вызывающий deleteAffairCallback c id Affair
+   const deleteCallback = () => deleteAffairCallback(affair._id)
 
-    const nameClass = s.name + ' ' + s2[props.affair.priority]
-    const buttonClass = s.closeButton + ' ' + s2[props.affair.priority]
-    const affairClass = s.affair + ' ' + s2[props.affair.priority]
+   const nameClass = s.name + ' ' + s2[affair.priority]
+   const buttonClass = s.closeButton + ' ' + s2[affair.priority]
+   const affairClass = s.affair + ' ' + s2[affair.priority]
 
-    return (
-        <div
-            id={'hw2-affair-' + props.affair._id}
-            className={affairClass}
-        >
-            <div id={'hw2-name-' + props.affair._id} className={nameClass}>
-                {/*создаёт студент*/}
+   return (
+      <div
+         id={'hw2-affair-' + affair._id}
+         className={affairClass}
+      >
+         <div id={'hw2-name-' + affair._id} className={nameClass}>
+            {affair.name}
+         </div>
+         <div id={'hw2-priority-' + affair._id} hidden>
+            {affair.priority}
+         </div>
 
-                {/**/}
-            </div>
-            <div id={'hw2-priority-' + props.affair._id} hidden>
-                {props.affair.priority}
-            </div>
-
-            <button
-                id={'hw2-button-delete-' + props.affair._id}
-                className={buttonClass}
-                // need to fix
-
-            >
-                {/*текст кнопки могут изменить студенты*/}
-                X
-                {/**/}
-            </button>
-        </div>
-    )
+         <button
+            id={'hw2-button-delete-' + affair._id}
+            className={buttonClass}
+            onClick={deleteCallback}
+         >
+            X
+         </button>
+      </div>
+   )
 }
-
-export default Affair
