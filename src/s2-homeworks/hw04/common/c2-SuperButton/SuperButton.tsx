3103d16dd5b64fe200e69bdd8b4c3d0da37aa0f2
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-04-20 03:12:05 +0400

    Finished: SuperButton

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -3,34 +3,33 @@ import s from './SuperButton.module.css'
 
 // тип пропсов обычной кнопки, children в котором храниться название кнопки там уже описан
 type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement>
+   HTMLButtonElement>
 
 type SuperButtonPropsType = DefaultButtonPropsType & {
-    xType?: string
+   xType?: string
 }
 
 const SuperButton: React.FC<SuperButtonPropsType> = (
-    {
-        xType,
-        className,
-        disabled,
-        ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
-    }
+   {
+      xType,
+      className,
+      disabled,
+      ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
+   }
 ) => {
-    const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+   const finalClassName = s.button + (className ? '' + className : ' ') +
+      (disabled ? s.disabled
+         : xType === 'red' ? s.red
+            : xType === 'secondary' ? s.secondary
+               : s.default)
 
-    return (
-        <button
-            disabled={disabled}
-            className={finalClassName}
-            {...restProps} // отдаём кнопке остальные пропсы если они есть (children там внутри)
-        />
-    )
+   return (
+      <button
+         disabled={disabled}
+         className={finalClassName}
+         {...restProps}
+      />
+   )
 }
 
 export default SuperButton
