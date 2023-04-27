f0db47b5fd4fca593a74312a0ecc2077dda8f1a9
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-04-27 04:46:43 +0400

    Const non-default

--- src/s2-homeworks/hw05/HW5.tsx
+++ src/s2-homeworks/hw05/HW5.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { HashRouter } from 'react-router-dom'
-import { Layout } from './layout/Layout'
+import {HashRouter} from 'react-router-dom'
+import {Layout} from './layout/Layout'
 import Pages from './Pages'
 
 /*
@@ -9,15 +9,13 @@ import Pages from './Pages'
 * 3 - застилизовать хэдэр и сайдбар в соответствии с дизайном
 * */
 
-function HW5() {
-    return (
-        <HashRouter>
-            {/*в gh-pages лучше работает HashRouter, с BrowserRouter скорее всего не пройдёт тест*/}
-            <Layout>
-                <Pages />
-            </Layout>
-        </HashRouter>
-    )
+export const HW5 = () => {
+   return (
+      <HashRouter>
+         {/*в gh-pages лучше работает HashRouter, с BrowserRouter скорее всего не пройдёт тест*/}
+         <Layout>
+            <Pages />
+         </Layout>
+      </HashRouter>
+   )
 }
-
-export default HW5
