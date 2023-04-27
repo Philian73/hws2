683e0a0c1ebef85d565d4d16d42aea2b3d0cfee6
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-04-27 04:48:47 +0400

    Added container class

--- src/s2-homeworks/hw05/header/Header.tsx
+++ src/s2-homeworks/hw05/header/Header.tsx
@@ -5,34 +5,36 @@ import {useLocation} from 'react-router-dom'
 import {PATH} from '../Pages'
 
 type PropsType = {
-    handleOpen: () => void
+   handleOpen: () => void
 }
 
 export const Header: FC<PropsType> = ({handleOpen}) => {
-    // hw5-menu изначально отсутствует, при нажатии на бургер - появляется, при повторном нажатии исчезает
-    const location = useLocation()
-    const currentPath = location.pathname
+   // hw5-menu изначально отсутствует, при нажатии на бургер - появляется, при повторном нажатии исчезает
+   const location = useLocation()
+   const currentPath = location.pathname
 
-    const pageName =
-        currentPath === PATH.PRE_JUNIOR
-            ? 'Pre-junior'
-            : currentPath === PATH.JUNIOR
-                ? 'Junior'
-                : currentPath === PATH.JUNIOR_PLUS
-                    ? 'Junior Plus'
-                    : 'Error'
-    return (
-        <>
-            <div id={'hw5-header'} className={s.header}>
-                <img
-                    src={burgerIcon}
-                    id={'hw5-burger-menu'}
-                    className={s.burgerMenuIcon}
-                    onClick={handleOpen}
-                    alt={'open menu'}
-                />
-                <h1>{pageName}</h1>
+   const pageName =
+      currentPath === PATH.PRE_JUNIOR
+         ? 'Pre-junior'
+         : currentPath === PATH.JUNIOR
+            ? 'Junior'
+            : currentPath === PATH.JUNIOR_PLUS
+               ? 'Junior Plus'
+               : 'Error'
+   return (
+      <>
+         <header id={'hw5-header'} className={s.header}>
+            <div className={s.headerContainer}>
+               <img
+                  src={burgerIcon}
+                  id={'hw5-burger-menu'}
+                  className={s.burgerMenuIcon}
+                  onClick={handleOpen}
+                  alt={'open menu'}
+               />
+               <h1>{pageName}</h1>
             </div>
-        </>
-    )
+         </header>
+      </>
+   )
 }
--- src/s2-homeworks/hw05/layout/Layout.tsx
+++ src/s2-homeworks/hw05/layout/Layout.tsx
@@ -1,29 +1,29 @@
-import React, { FC, ReactNode, useEffect, useState } from 'react'
-import { Header } from '../header/Header'
-import { Sidebar } from '../sidebar/Sidebar'
+import React, {FC, ReactNode, useEffect, useState} from 'react'
+import {Header} from '../header/Header'
+import {Sidebar} from '../sidebar/Sidebar'
 
 type PropsType = {
-    children: ReactNode
+   children: ReactNode
 }
 
-export const Layout: FC<PropsType> = ({ children }) => {
-    const [open, setOpen] = useState(false)
-    const handleClose = () => setOpen(false)
-    const handleOpen = () => setOpen(true)
+export const Layout: FC<PropsType> = ({children}) => {
+   const [open, setOpen] = useState(false)
+   const handleClose = () => setOpen(false)
+   const handleOpen = () => setOpen(true)
 
-    useEffect(() => {
-        open && (document.body.style.overflow = 'hidden')
-        !open && (document.body.style.overflow = 'unset')
-    }, [open]) // отключает прокрутку при открытом меню
+   useEffect(() => {
+      open && (document.body.style.overflow = 'hidden')
+      !open && (document.body.style.overflow = 'unset')
+   }, [open]) // отключает прокрутку при открытом меню
 
-    return (
-        <>
-            <Sidebar open={open} handleClose={handleClose} />
-            <Header handleOpen={handleOpen} />
-            <div>
-                {/*страницы*/}
-                {children}
-            </div>
-        </>
-    )
+   return (
+      <>
+         <Sidebar open={open} handleClose={handleClose} />
+         <Header handleOpen={handleOpen} />
+         <div className="container">
+            {/*страницы*/}
+            {children}
+         </div>
+      </>
+   )
 }
