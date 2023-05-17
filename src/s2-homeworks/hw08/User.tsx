99674312b36713d0410fa168e2a29d15fa0aed7b
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-05-17 23:31:51 +0400

    Made a display of users

--- src/s2-homeworks/hw08/User.tsx
+++ src/s2-homeworks/hw08/User.tsx
@@ -1,25 +1,23 @@
 import React from 'react'
-import { UserType } from './HW8'
+import {UserType} from './HW8'
 import s from './HW8.module.css'
 
 // types
 type UserPropsType = {
-    u: UserType
+   u: UserType
 }
 
-const User: React.FC<UserPropsType> = ({ u }) => {
-    return (
-        <tr id={'hw8-user-' + u._id + '-' + u.age} className={s.item}>
-            <td id={'hw8-user-name-' + u._id} className={s.nameCol}>
-                {/*отобразить имя*/}
-
-            </td>
-            <td id={'hw8-user-age-' + u._id}>
-                {/*отобразить возраст*/}
-
-            </td>
-        </tr>
-    )
+const User: React.FC<UserPropsType> = ({u}) => {
+   return (
+      <tr id={'hw8-user-' + u._id + '-' + u.age} className={s.item}>
+         <td id={'hw8-user-name-' + u._id} className={s.nameCol}>
+            {u.name}
+         </td>
+         <td id={'hw8-user-age-' + u._id} className={s.ageCol}>
+            {u.age}
+         </td>
+      </tr>
+   )
 }
 
 export default User
