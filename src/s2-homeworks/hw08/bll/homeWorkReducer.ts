4cfc8b386c0f34274adfae99e6c2eac011f62b73
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-05-17 23:30:25 +0400

    Wrote the logic of the reducer

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,20 @@
 import {UserType} from '../HW8'
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+   | { type: 'sort'; payload: 'up' | 'down' }
+   | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
-    }
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
+   switch (action.type) {
+      case 'sort': {
+         return [...state].sort((a, b) => action.payload === 'up'
+            ? a.name.localeCompare(b.name)
+            : b.name.localeCompare(a.name))
+      }
+      case 'check': {
+         return state.filter(user => user.age >= action.payload)
+      }
+      default:
+         return state
+   }
 }
