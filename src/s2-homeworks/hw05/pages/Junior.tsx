43964e45c6add1e9188bdd202a3173ca974cbabc
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-05-27 23:36:02 +0300

    import HW-9

--- src/s2-homeworks/hw05/pages/Junior.tsx
+++ src/s2-homeworks/hw05/pages/Junior.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import {HW6} from '../../hw06/HW6'
-import {HW7} from '../../hw07/HW7'
-import {HW8} from '../../hw08/HW8'
-// import {HW9} from '../../hw09/HW9'
+import { HW6 } from '../../hw06/HW6'
+import { HW7 } from '../../hw07/HW7'
+import { HW8 } from '../../hw08/HW8'
+import { HW9 } from '../../hw09/HW9'
 
 export const Junior: React.FC = () => {
    return (
@@ -10,7 +10,7 @@ export const Junior: React.FC = () => {
          <HW6 />
          <HW7 />
          <HW8 />
-         {/*<HW9 />*/}
+         <HW9 />
       </div>
    )
 }
