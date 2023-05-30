54f77d55d1b2e19c8317470382a91490ec577366
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-05-31 00:38:13 +0400

    import HW10

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-// import {HW10} from '../../hw10/HW10'
+
+import { HW10 } from '../../hw10/HW10'
 // import {HW11} from '../../hw11/HW11'
 // import {HW12} from '../../hw12/HW12'
 // import {HW13} from '../../hw13/HW13'
@@ -7,14 +8,14 @@ import React from 'react'
 // import {HW15} from '../../hw15/HW15'
 
 export const JuniorPlus: React.FC = () => {
-    return (
-        <div id={'hw5-page-junior-plus'}>
-            {/*<HW10 />*/}
-            {/*<HW11 />*/}
-            {/*<HW12 />*/}
-            {/*<HW13 />*/}
-            {/*<HW14 />*/}
-            {/*<HW15 />*/}
-        </div>
-    )
+   return (
+      <div id={'hw5-page-junior-plus'}>
+         <HW10 />
+         {/*<HW11 />*/}
+         {/*<HW12 />*/}
+         {/*<HW13 />*/}
+         {/*<HW14 />*/}
+         {/*<HW15 />*/}
+      </div>
+   )
 }
