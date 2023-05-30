4def5757805f0ffa44e364bb21eb655d5aeb35b2
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-05-31 00:38:50 +0400

    Added CHANGE_LOADING and typed

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,22 +1,25 @@
-const initState = {
-    isLoading: false,
-}
-
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // пишет студент  // need to fix
-
-        default:
-            return state
-    }
-}
-
 type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
+   type: 'CHANGE_LOADING'
+   isLoading: boolean
+}
+
+type InitialStateType = typeof initState
+
+const initState = {
+   isLoading: false,
+}
+
+export const loadingReducer = (state = initState, action: LoadingActionType): InitialStateType => { // fix any
+   switch (action.type) {
+      case 'CHANGE_LOADING':
+         return { ...state, isLoading: action.isLoading }
+
+      default:
+         return state
+   }
 }
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
-    type: 'CHANGE_LOADING',
-    isLoading,
+   type: 'CHANGE_LOADING',
+   isLoading,
 })
