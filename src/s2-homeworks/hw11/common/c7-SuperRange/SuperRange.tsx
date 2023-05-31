730eddd0ddf2d618368adb3b6445e7a52b8e57f0
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-06-01 01:20:16 +0400

    refactor

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -4,28 +4,34 @@ import { Slider, SliderProps } from '@mui/material'
 const SuperRange: React.FC<SliderProps> = (props) => {
    return (
       <Slider
-         sx={{ // стили для слайдера
+         sx={{ // стили для слайдера // пишет студент
+            width: 160,
             color: '#00CC22',
-            width: '10.4125rem',
-            '& .MuiSlider-thumb': {
-               backgroundColor: '#fff',
-               border: '1px solid #00CC22',
-               borderRadius: '50%',
-               width: '1.125rem',
-               height: '1.125rem',
-               '&::before': {
-                  backgroundColor: '#00CC22',
-                  width: '0.375rem',
-                  height: '0.375rem',
-               },
-               transition: 'none', // Изменяем анимацию передвижения ползунка
+            '& .MuiSlider-track': {
+               border: 'none',
+               transition: 'none', // Изменяем анимацию заполнения
             },
             '& .MuiSlider-rail': {
                backgroundColor: '#8B8B8B', // Изменяем цвет неиспользуемой части слайдера
                opacity: 1,
             },
-            '& .MuiSlider-track': {
-               transition: 'none', // Изменяем анимацию заполнения
+            '& .MuiSlider-thumb': {
+               width: 20,
+               height: 20,
+               border: '1px solid #00CC22',
+               backgroundColor: '#fff',
+               '&:before': {
+                  content: '""',
+                  width: 6,
+                  height: 6,
+                  borderRadius: '50%',
+                  backgroundColor: '#00CC22',
+                  zIndex: -1,
+               },
+               '&:hover, &.Mui-focusVisible, &.Mui-active': {
+                  boxShadow: 'none',
+               },
+               transition: 'none', // Изменяем анимацию передвижения ползунка
             },
          }}
          {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
