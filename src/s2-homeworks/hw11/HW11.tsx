635675a84ebdc60d37fe26e4d47828663e4574fa
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-06-01 00:39:58 +0400

    HW11 completed

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -4,48 +4,48 @@ import s2 from '../../s1-main/App.module.css'
 import { restoreState } from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 
-/*
-* 1 - передать значения в оба слайдера
-* 2 - дописать типы и логику функции change
-* 3 - сделать стили в соответствии с дизайном
-* */
+export const HW11 = () => {
+   // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
+   const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
+   const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-function HW11() {
-    // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
-    const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
-    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
+   const change = (event: Event, value: number | number[]) => {
+      // если пришёл массив - сохранить значения в оба useState, иначе в первый
+      if (Array.isArray(value)) {
+         setValue1(value[0])
+         value[1] > value[0] && setValue2(value[1])
+      } else {
+         setValue1(value)
+      }
+   }
 
-    const change = (event: any, value: any) => {
-        // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
-    }
+   return (
+      <div id={'hw11'}>
+         <div className={s2.hwTitle}>Homework #11</div>
 
-    return (
-        <div id={'hw11'}>
-            <div className={s2.hwTitle}>Homework #11</div>
-
-            <div className={s2.hw}>
-                <div className={s.container}>
-                    <div className={s.wrapper}>
-                        <span id={'hw11-value'} className={s.number}>{value1}</span>
-                        <SuperRange
-                            id={'hw11-single-slider'}
-                            // сделать так чтоб value1 изменялось // пишет студент
-
-                        />
-                    </div>
-                    <div className={s.wrapper}>
-                        <span id={'hw11-value-1'} className={s.number}>{value1}</span>
-                        <SuperRange
-                            id={'hw11-double-slider'}
-                            // сделать так чтоб value1/2 изменялось // пишет студент
-
-                        />
-                        <span id={'hw11-value-2'} className={s.number}>{value2}</span>
-                    </div>
-                </div>
+         <div className={`${s2.hw} ${s2.hw_11}`}>
+            <div className={s.container}>
+               <div className={s.wrapper}>
+                  <span id={'hw11-value'} className={s.number}>{value1}</span>
+                  <SuperRange
+                     id={'hw11-single-slider'}
+                     value={value1}
+                     // value 1 изменяется
+                     onChange={change}
+                  />
+               </div>
+               <div className={s.wrapper}>
+                  <span id={'hw11-value-1'} className={s.number}>{value1}</span>
+                  <SuperRange
+                     id={'hw11-double-slider'}
+                     value={[value1, value2]}
+                     // value1/2 изменяются
+                     onChange={change}
+                  />
+                  <span id={'hw11-value-2'} className={s.number}>{value2}</span>
+               </div>
             </div>
-        </div>
-    )
+         </div>
+      </div>
+   )
 }
-
-export default HW11
