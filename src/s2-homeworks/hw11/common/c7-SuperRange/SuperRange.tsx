ab2449aa2a6f20f587205b2d055bcc722cee8fe2
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-06-01 00:41:10 +0400

    Styles (SX) for Slider from MUI

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,15 +1,36 @@
 import React from 'react'
-import {Slider, SliderProps} from '@mui/material'
+import { Slider, SliderProps } from '@mui/material'
 
 const SuperRange: React.FC<SliderProps> = (props) => {
-    return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
-        />
-    )
+   return (
+      <Slider
+         sx={{ // стили для слайдера
+            color: '#00CC22',
+            width: '10.3125rem',
+            '& .MuiSlider-thumb': {
+               backgroundColor: '#fff',
+               border: '1px solid #00CC22',
+               borderRadius: '50%',
+               width: '1.125rem',
+               height: '1.125rem',
+               '&::before': {
+                  backgroundColor: '#00CC22',
+                  width: '0.375rem',
+                  height: '0.375rem',
+               },
+               transition: 'none', // Изменяем анимацию передвижения ползунка
+            },
+            '& .MuiSlider-rail': {
+               backgroundColor: '#8B8B8B', // Изменяем цвет неиспользуемой части слайдера
+               opacity: 1,
+            },
+            '& .MuiSlider-track': {
+               transition: 'none', // Изменяем анимацию заполнения
+            },
+         }}
+         {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+      />
+   )
 }
 
 export default SuperRange
